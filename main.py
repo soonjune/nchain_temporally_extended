6a6fdc075fad00e36e048e859ae279cdb9c2bb4d
soonjune <tino7060@likelion.org>
2021-08-17 13:26:01 +0900

    q-learning agent

--- main.py
+++ main.py
@@ -4,26 +4,33 @@ import matplotlib.pyplot as plt
 from learning_agents import QLearningAgent
 from value_iteration_agent import NChainVIAgent
 import numpy as np
+from Q_learning_agent import Agent
 
-env = NChainEnv(max_steps=100)
-n_games = 1000
-last_10_avgs = []
-rewards = []
+if __name__ == '__main__':
+    env = NChainEnv(max_steps=100)
+    agent = Agent(lr=0.001, gamma=0.9, eps_start=1.0, eps_end=0.01,
+    eps_dec=0.9999995, n_actions=2, n_states=10)
 
-for i in range(1000):
-    done = False
-    obs = env.reset()
-    reward = 0
-    while not done:
-        action = env.action_space.sample()
-        obs, reward, done, info = env.step(action)
-    rewards.append(reward)
+    n_games = 1000
+    last_10_avgs = []
+    rewards = []
 
-    if i % 10 == 0:
-        average = np.mean(rewards[-10:])
-        last_10_avgs.append(average)
-plt.plot(last_10_avgs)
-plt.show()
+    for i in range(n_games):
+        done = False
+        obs = env.reset()
+        reward = 0
+        while not done:
+            action = agent.choose_action(obs)
+            obs_, reward, done, info = env.step(action)
+            agent.learn(obs, action, reward, obs_)
+            obs = obs_
+        rewards.append(reward)
+
+        if i % 100 == 0:
+            average = np.mean(rewards[-10:])
+            last_10_avgs.append(average)
+    plt.plot(last_10_avgs)
+    plt.show()
 
 # # hyperparameters tuned for NChain
 # epsilon = 0.1
